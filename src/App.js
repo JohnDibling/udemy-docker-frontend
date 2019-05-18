83f317766123edfe1d1f3d836e21c4d486eca5d3
John Dibling <john.dibling@thinkpiecepartners.com>
2019-05-18 08:10:51 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
